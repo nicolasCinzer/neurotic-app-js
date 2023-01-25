273fd9c5f6c642d31a98ebf2701a30873ec5f737
Nicolas Cinzer <ncinzer@os.aeroterra.com>
2023-01-24 23:44:28 -0500

    clean gym

--- app/gym/page.jsx
+++ app/gym/page.jsx
@@ -1,11 +1,5 @@
-import BubbleBudget from 'components/BubbleBudget';
 import gymStyle from './Gym.module.css';
 
-
 export default function Gym() {
-  return (
-    <div className={gymStyle.gymBody}>
-      <BubbleBudget />
-    </div>
-  );
+  return <div className={gymStyle.gymBody}></div>;
 }
