5fb2ac6cbe04dd682adb521e88eb52f6ef3a3621
Nicolas Cinzer <nicolas.cinzer00@gmail.com>
2023-01-29 23:01:09 -0500

    Move directories

--- components/budget/layout/Filters.jsx
+++
@@ -1,9 +0,0 @@
-import filtersStyle from './css/Filters.module.css';
-
-export default function Filters() {
-  return (
-    <div className={filtersStyle.filtersBody}>
-      Filters <button className={filtersStyle.addFilter}>+</button>
-    </div>
-  );
-}
--- components/budget/layout/Tag.jsx
+++
@@ -1,5 +0,0 @@
-import tagStyle from './css/Tag.module.css';
-
-export default function Tag({ tag }) {
-  return <div className={tagStyle.tag}>{tag}</div>;
-}
