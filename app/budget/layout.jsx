12c75a1a63b5dbce2e1f0466f65d73a1ebd2efce
Nicolas Cinzer <nicolas.cinzer00@gmail.com>
2023-02-01 00:00:51 -0500

    API call

--- app/budget/layout.jsx
+++ app/budget/layout.jsx
@@ -3,14 +3,24 @@ import Sidebar from 'components/budget/layout/sidebar/Sidebar';
 
 import { dm_sans } from '../../components/font';
 
-export default function BudgetLayout({ children }) {
+const getBudgetGeneralInfo = () => {
+  return fetch('https://my-json-server.typicode.com/nicolasCinzer/neurotic-fake-api/budget').then(res => res.json());
+};
+
+export default async function BudgetLayout({ children }) {
+  const budgetInfo = await getBudgetGeneralInfo();
+
   return (
     <>
       <div
         className={dm_sans.variable}
         style={{ padding: '32px', display: 'grid', gridTemplateColumns: '2fr 10fr', gap: '20px' }}
       >
-        <Sidebar />
+        <Sidebar
+          debts={budgetInfo.debtors}
+          wallets={budgetInfo.wallets}
+          currentBudg={budgetInfo.totalBudget}
+        />
         {children}
       </div>
     </>
