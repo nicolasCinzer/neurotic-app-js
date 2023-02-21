787f6cdd2c1b057a27b8340c0f789698fe9396b2
Nicolas Cinzer <nicolas.cinzer00@gmail.com>
2023-02-20 23:06:46 -0500

    Debts style

--- components/budget/layout/sidebar/Debts.jsx
+++ components/budget/layout/sidebar/Debts.jsx
@@ -1,16 +1,22 @@
+import debtsStyle from './css/Debts.module.css';
+import { useRouter } from 'next/navigation';
+
 export default function Debts({ debts }) {
-  console.log(debts);
+  const router = useRouter();
 
   return (
-    <div>
+    <div className={debtsStyle.debtBody}>
       Debts
       {Object.keys(debts).map(debtor => {
-        console.log(debtor);
         return (
-          <div>
-            {debtor}
-            {debts[debtor].debt}
-            {debts[debtor].lastDate}
+          <div
+            className={debtsStyle.debtItem}
+            onClick={() => router.push(`/budget/debts/${debtor}`)}
+            key={debtor}
+          >
+            <div className={debtsStyle.debtTitle}>{debtor}</div>
+            <div className={debtsStyle.debtMoney}>{debts[debtor].debt}</div>
+            <div className={debtsStyle.debtDate}>{new Date(debts[debtor].lastDate * 1000).toLocaleDateString('en-US')}</div>
           </div>
         );
       })}
