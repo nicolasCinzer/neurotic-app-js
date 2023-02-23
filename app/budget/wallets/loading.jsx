bf16b0f58d4babca9166b66485d01a5bbf9679ef
Nicolas Cinzer <nicolas.cinzer00@gmail.com>
2023-02-22 20:38:58 -0500

    loadings

---
+++ app/budget/wallets/loading.jsx
@@ -0,0 +1,19 @@
+export default function Loading() {
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        display: 'flex',
+        alignItems: 'center',
+        fontSize: '26px',
+        fontWeight: 'bold',
+        letterSpacing: '10px',
+        color: '#4b4b4b',
+      }}
+    >
+      LOADING...
+    </div>
+  );
+}
